182c9371f526f14d16688527fd3aadecba5a67ad
Abdul Ghodir <abdulghodir08@gmail.com>
2024-10-04 15:09:57 +0700

    TebakAngka

--- RandomGame.cpp
+++ RandomGame.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
+
 
 int main(){
     cout << "=========================================================================" << endl
---
+++ TebakAngka.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void tebakAagka(){
+    
+}
