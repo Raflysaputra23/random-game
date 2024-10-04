7b25f6ab4f2dc58589a1e70075f26202c545a70d
Abdul Ghodir <abdulghodir08@gmail.com>
2024-10-04 14:56:17 +0700

    Buat game tebak angka

--- RandomGame.cpp
+++ RandomGame.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main(){
+    cout << "=========================================================================" << endl
+         << "\t" "\t" "\t" "\t" << "Selamat Datang" << endl
+         << "\t" "\t" "\t" << "       Di Game Kehokian" << endl
+         << "=========================================================================" << endl << endl;
 
+    cout << "Buktikan Keberuntungan Anda Dengan Bermain Game Ini" << endl;
+    cout << "Silahkan Masukkan Nama Anda" << endl;
 
     return 0;
 }
