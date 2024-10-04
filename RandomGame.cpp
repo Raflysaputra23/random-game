1393f71f5d43f572da0787a0c61258815b77aec1
Abdul Ghodir <abdulghodir08@gmail.com>
2024-10-05 00:18:23 +0700

    Menu Baru

--- RandomGame.cpp
+++ RandomGame.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 
 
+
+
 int main(){
+    string nama;
+    string computer;
+
     cout << "=========================================================================" << endl
          << "\t" "\t" "\t" "\t" << "Selamat Datang" << endl
          << "\t" "\t" "\t" << "       Di Game Kehokian" << endl
          << "=========================================================================" << endl << endl;
 
-    cout << "Buktikan Keberuntungan Anda Dengan Bermain Game Ini" << endl;
-    cout << "Silahkan Masukkan Nama Anda" << endl;
+    cout << "Buktikan Keberuntungan Anda Dengan Bermain Game Ini" << endl << endl;
+
+    cout << "Masukkan Nama Anda : ";
+    getline(cin,nama);
+    cout << "Masukkan Nama Lawan Anda : ";
+    getline(cin,computer);
+    cout << endl;
+
+    cout << computer << " : " << '"' << "Haloo " << nama << " senang bertemu denganmu pekenalkan namaku " << computer << '"';
+    cin.get();
+    teksComputer(computer, "Mari kita lihat siapa yang paling hebat dalam hal keberuntungan");
+    cin.get();
+    teksComputer(computer, "Game apa yang ingin kamu mainkan");
+    cout << endl;
 
     return 0;
 }
--- TebakAngka.cpp
+++ TebakAngka.cpp
@@ -2,6 +2,53 @@
 #include <ctime>
 using namespace std;
 
-void tebakAagka(){
-    
+void teksComputer(string computer, string teks){
+    cout << computer << " : " << '"' << teks << '"';
+}
+
+int main(){
+    int random;
+    srand(time(0));
+    random = 1 + rand() % 100;
+
+    int a;
+    string nama;
+    string computer;
+
+    cout << "=========================================================================" << endl
+         << "\t" "\t" "\t" "\t" << "Selamat Datang" << endl
+         << "\t" "\t" "\t" << "       Di Game Kehokian" << endl
+         << "=========================================================================" << endl << endl;
+
+    cout << "Buktikan Keberuntungan Anda Dengan Bermain Game Ini" << endl << endl;
+
+    cout << "Masukkan Nama Anda : ";
+    getline(cin,nama);
+    cout << "Masukkan Nama Lawan Anda : ";
+    getline(cin,computer);
+    cout << endl;
+
+    cout << computer << " : " << '"' << "Haloo " << nama << " senang bertemu denganmu pekenalkan namaku " << computer << '"';
+    cin.get();
+    teksComputer(computer, "Mari kita lihat siapa yang paling hebat dalam hal keberuntungan");
+    cin.get();
+    teksComputer(computer, "Game apa yang ingin kamu mainkan");
+    cout << endl;
+
+    cin.get();
+    teksComputer(computer, "Baiklah kita akan bermain tebak tebakan angka");
+    cin.get();
+    teksComputer(computer, "Aku dulu disebut sebagai penerawang handal jadi jangan meremehkanku");
+    cout << endl << endl;
+
+    //Codingan tebak tebakan
+    cout << "==================================" << endl
+         << "Game Tebak Angka" << endl
+         << "==================================" << endl << endl;
+    cout << "Tebak angka yang dikeluarkan oleh Tako-chan" << endl
+         << "Tebakan yang paling mendekati adalah yang menang" << endl;
+    cin.get();
+
+
+    return 0;
 }
